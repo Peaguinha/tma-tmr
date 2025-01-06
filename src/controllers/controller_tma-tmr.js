c5fb0d30a721cb633da5aa475ba23279b8187f62
Pedro Henrique de Almeida Peixoto <peixotopedro815@hotmail.com>
2025-01-06 14:53:18 -0300

    adição de atributos para o controller

---
+++ src/controllers/controller_tma-tmr.js
@@ -0,0 +1,19 @@
+import { adicionarNaPlanilha } from "../services/enviar_planilha.js";
+
+export async function createdata(req, res) {
+    const { nome_atendente, telefone, data_atendimento, hora_inicio_atendimento, hora_fim_atendimento } = req.body;
+
+    // Dados a serem enviados para a planilha
+    const dados = [nome_atendente, telefone, data_atendimento, hora_inicio_atendimento, hora_fim_atendimento];
+
+    console.log("dados:", dados);
+    
+    // Chama o service para adicionar os dados à planilha
+    const sucesso = await adicionarNaPlanilha(dados);
+
+    if (sucesso) {
+        return res.status(200).json({ message: "Dados adicionados com sucesso!" });
+    } else {
+        return res.status(500).json({ message: "Erro ao adicionar dados." });
+    }
+}
