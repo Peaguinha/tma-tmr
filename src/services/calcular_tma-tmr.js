e6841d5201a6f28daf838e356723036b4185dc31
Pedro Henrique de Almeida Peixoto <peixotopedro815@hotmail.com>
2025-01-06 15:10:16 -0300

    configuração do service calcular_tma-tmr

---
+++ src/services/calcular_tma-tmr.js
@@ -0,0 +1,15 @@
+function calcularDiferencaEmMinutos(inicio, fim) {
+    const inicioData = new Date(`${inicio}`);
+    const fimData = new Date(`${fim}`);
+    return (fimData - inicioData) / 60000; // Retorna a diferença em minutos
+}
+
+// Função para calcular TMA
+export function calcularTMA(horaInicio, horaFim) {
+    return calcularDiferencaEmMinutos(horaInicio, horaFim);
+}
+
+// Função para calcular TMR
+export function calcularTMR(envioSolicitacao, respostaRecebida) {
+    return calcularDiferencaEmMinutos(envioSolicitacao, respostaRecebida);
+}
