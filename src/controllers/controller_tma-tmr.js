66d7231a9c4b60671ba34146b8116f0b23e05a50
Pedro Henrique de Almeida Peixoto <peixotopedro815@hotmail.com>
2025-01-07 10:44:37 -0300

    alterações no controller e no service de calculo do tma e tmr

--- src/controllers/controller_tma-tmr.js
+++ src/controllers/controller_tma-tmr.js
@@ -4,25 +4,29 @@ import { calcularTMA, calcularTMR } from "../services/calcular_tma-tmr.js";
 export async function createdata(req, res) {
     const {
         nome_atendente,
-        Telefone,
+        telefone,
         data_atendimento,
         hora_ini_atendimento,
         hora_fim_atendimento,
+        envio_solicitacao, // Deve ser enviado no JSON do request
+        resposta_recebida  // Deve ser enviado no JSON do request
     } = req.body;
 
     // Calcula TMA e TMR
     const tma = calcularTMA(hora_ini_atendimento, hora_fim_atendimento);
-    const tmr = calcularTMR(hora_ini_atendimento, hora_fim_atendimento);
+    const tmr = calcularTMR(envio_solicitacao, resposta_recebida);
 
     // Dados a serem enviados para a planilha
     const dados = [
         nome_atendente,
-        Telefone,
+        telefone,
         data_atendimento,
         hora_ini_atendimento,
         hora_fim_atendimento,
+        envio_solicitacao,
+        resposta_recebida,
         tma,
-        tmr,
+        tmr
     ];
 
     console.log("dados:", dados);
@@ -31,7 +35,11 @@ export async function createdata(req, res) {
     const sucesso = await adicionarNaPlanilha(dados);
 
     if (sucesso) {
-        return res.status(200).json({ message: "Dados adicionados com sucesso!" });
+        return res.status(200).json({
+            message: "Dados adicionados com sucesso!",
+            tma,
+            tmr
+        });
     } else {
         return res.status(500).json({ message: "Erro ao adicionar dados." });
     }
