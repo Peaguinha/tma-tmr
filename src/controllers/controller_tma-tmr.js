4ec1f8651ab6eaef2aae9abde5b313a19cc51b70
Pedro Henrique de Almeida Peixoto <peixotopedro815@hotmail.com>
2025-01-07 13:21:04 -0300

    alteração no controller 3.0

--- src/controllers/controller_tma-tmr.js
+++ src/controllers/controller_tma-tmr.js
@@ -7,19 +7,19 @@ export async function createdata(req, res) {
         telefone,
         data_atendimento,
         hora_fechou_robo,
-        hora_ini_atendimento_humano,
-        hora_fim_atendimento_humano
+        hora_ini_atendimento,  // Renomeado para 'hora_ini_atendimento'
+        hora_fim_atendimento    // Renomeado para 'hora_fim_atendimento'
     } = req.body;
 
     try {
         // Verificar se as horas foram enviadas corretamente
-        if (!hora_fechou_robo || !hora_ini_atendimento_humano || !hora_fim_atendimento_humano) {
-            return res.status(400).json({ message: "Hora de fechamento do robô, início ou fim do atendimento humano não fornecida." });
+        if (!hora_fechou_robo || !hora_ini_atendimento || !hora_fim_atendimento) {
+            return res.status(400).json({ message: "Hora de fechamento do robô, início ou fim do atendimento não fornecida." });
         }
 
         // Calcula TMA e TMR
-        const tma = calcularTMA(hora_ini_atendimento_humano, hora_fim_atendimento_humano);
-        const tmr = calcularTMR(hora_fechou_robo, hora_ini_atendimento_humano);
+        const tma = calcularTMA(hora_ini_atendimento, hora_fim_atendimento);
+        const tmr = calcularTMR(hora_fechou_robo, hora_ini_atendimento);
 
         // Dados a serem enviados para a planilha
         const dados = [
@@ -27,8 +27,8 @@ export async function createdata(req, res) {
             telefone,
             data_atendimento,
             hora_fechou_robo,
-            hora_ini_atendimento_humano,
-            hora_fim_atendimento_humano,
+            hora_ini_atendimento,
+            hora_fim_atendimento,
             tma,
             tmr
         ];
